326804ebd3edd3f9842c994d85f1934ac3688385
Jeanlui21 <40554237+Jeanlui21@users.noreply.github.com>
2019-08-20 02:32:30 -0500

    Propuesto02

---
+++ Propuesto02/Session_02/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Session_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.CboAnio = new System.Windows.Forms.ComboBox();
+            this.DgPedidos = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.CboMes = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DgPedidos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AÑOS:";
+            // 
+            // CboAnio
+            // 
+            this.CboAnio.FormattingEnabled = true;
+            this.CboAnio.Location = new System.Drawing.Point(162, 28);
+            this.CboAnio.Name = "CboAnio";
+            this.CboAnio.Size = new System.Drawing.Size(121, 21);
+            this.CboAnio.TabIndex = 1;
+            this.CboAnio.SelectionChangeCommitted += new System.EventHandler(this.CboAnios_SelectionChangeCommitted);
+            // 
+            // DgPedidos
+            // 
+            this.DgPedidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgPedidos.Location = new System.Drawing.Point(13, 104);
+            this.DgPedidos.Name = "DgPedidos";
+            this.DgPedidos.Size = new System.Drawing.Size(775, 219);
+            this.DgPedidos.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Lista de Pedidos";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(459, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "MES:";
+            // 
+            // CboMes
+            // 
+            this.CboMes.FormattingEnabled = true;
+            this.CboMes.Location = new System.Drawing.Point(522, 28);
+            this.CboMes.Name = "CboMes";
+            this.CboMes.Size = new System.Drawing.Size(121, 21);
+            this.CboMes.TabIndex = 11;
+            this.CboMes.SelectionChangeCommitted += new System.EventHandler(this.CboMes_SelectionChangeCommitted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 368);
+            this.Controls.Add(this.CboMes);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.DgPedidos);
+            this.Controls.Add(this.CboAnio);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DgPedidos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CboAnio;
+        private System.Windows.Forms.DataGridView DgPedidos;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox CboMes;
+    }
+}
+
---
+++ Propuesto02/Session_02/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Session_02
+{
+    public partial class Form1 : Form
+    {
+      
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["Leon"].ConnectionString);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListaAnios();
+   
+        }
+
+
+        public void ListaAnios()
+        {
+            using (SqlCommand cmd = new SqlCommand("Usp_ListaAnios", cn))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    da.SelectCommand = cmd;
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    using (DataSet df = new DataSet())
+                    {
+                        da.Fill(df, "Usp_ListaAnios");
+                        CboAnio.DataSource = df.Tables["Usp_ListaAnios"];
+                        CboAnio.DisplayMember = "Anios";
+                        CboAnio.ValueMember = "Anios";
+                    }
+                }
+            }
+        }
+
+
+
+        private void CboAnios_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            using (SqlCommand cmd = new SqlCommand("Usp_ListaMeses", cn))
+            {
+                using (SqlDataAdapter Da = new SqlDataAdapter())
+                {
+                    Da.SelectCommand = cmd;
+                    Da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    Da.SelectCommand.Parameters.AddWithValue("@anio", CboAnio.SelectedValue);
+                    using (DataSet df = new DataSet())
+                    {
+              
+                        Da.Fill(df, "Usp_ListaMeses");
+                        CboMes.DataSource = df.Tables["Usp_ListaMeses"];
+                        CboMes.DisplayMember = "Mes";
+                        CboMes.ValueMember = "Mes";
+                     
+                    }
+                }
+            }
+        }
+
+        private void CboMes_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            using (SqlCommand cmd = new SqlCommand("Usp_Lista_PedidosxMesXAnioTest", cn))
+            {
+                using (SqlDataAdapter Da = new SqlDataAdapter())
+                {
+                    Da.SelectCommand = cmd;
+                    Da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    Da.SelectCommand.Parameters.AddWithValue("@anio", CboAnio.SelectedValue);
+                    Da.SelectCommand.Parameters.AddWithValue("@mes", CboMes.SelectedValue);
+                    using (DataSet df = new DataSet())
+                    {
+                        Da.Fill(df, "Usp_Lista_PedidosxMesXAnioTest");
+                        DgPedidos.DataSource = df.Tables["Usp_Lista_PedidosxMesXAnioTest"];
+                      
+                    }
+                }
+            }
+        }
+    }
+}
