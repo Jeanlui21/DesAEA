8086757651b150fea30bb448d7546277ebfab5e9
Jeanlui21 <40554237+Jeanlui21@users.noreply.github.com>
2019-08-20 00:24:45 -0500

    Propuesto001

---
+++ Propuesto01/Session_02/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Session_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.CboApe = new System.Windows.Forms.ComboBox();
+            this.DgPedidos = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LblNumero = new System.Windows.Forms.TextBox();
+            this.DgDetalle = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.LblMonto = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.CboNom = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DgPedidos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DgDetalle)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "APELLIDO:";
+            // 
+            // CboApe
+            // 
+            this.CboApe.FormattingEnabled = true;
+            this.CboApe.Location = new System.Drawing.Point(162, 28);
+            this.CboApe.Name = "CboApe";
+            this.CboApe.Size = new System.Drawing.Size(121, 21);
+            this.CboApe.TabIndex = 1;
+            this.CboApe.SelectionChangeCommitted += new System.EventHandler(this.CboAnios_SelectionChangeCommitted);
+            // 
+            // DgPedidos
+            // 
+            this.DgPedidos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgPedidos.Location = new System.Drawing.Point(13, 104);
+            this.DgPedidos.Name = "DgPedidos";
+            this.DgPedidos.Size = new System.Drawing.Size(775, 219);
+            this.DgPedidos.TabIndex = 2;
+            this.DgPedidos.DoubleClick += new System.EventHandler(this.DgPedidos_DoubleClick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(592, 351);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nro de Pedidos";
+            // 
+            // LblNumero
+            // 
+            this.LblNumero.Location = new System.Drawing.Point(688, 348);
+            this.LblNumero.Name = "LblNumero";
+            this.LblNumero.Size = new System.Drawing.Size(100, 20);
+            this.LblNumero.TabIndex = 4;
+            // 
+            // DgDetalle
+            // 
+            this.DgDetalle.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgDetalle.Location = new System.Drawing.Point(12, 399);
+            this.DgDetalle.Name = "DgDetalle";
+            this.DgDetalle.Size = new System.Drawing.Size(776, 287);
+            this.DgDetalle.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Lista de Pedidos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 374);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Lista de Detalles";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(592, 717);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Monto del Pedido";
+            // 
+            // LblMonto
+            // 
+            this.LblMonto.Location = new System.Drawing.Point(688, 714);
+            this.LblMonto.Name = "LblMonto";
+            this.LblMonto.Size = new System.Drawing.Size(100, 20);
+            this.LblMonto.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(459, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "NOMBRE:";
+            // 
+            // CboNom
+            // 
+            this.CboNom.FormattingEnabled = true;
+            this.CboNom.Location = new System.Drawing.Point(522, 28);
+            this.CboNom.Name = "CboNom";
+            this.CboNom.Size = new System.Drawing.Size(121, 21);
+            this.CboNom.TabIndex = 11;
+            this.CboNom.SelectionChangeCommitted += new System.EventHandler(this.CboNom_SelectionChangeCommitted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(804, 749);
+            this.Controls.Add(this.CboNom);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.LblMonto);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.DgDetalle);
+            this.Controls.Add(this.LblNumero);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DgPedidos);
+            this.Controls.Add(this.CboApe);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DgPedidos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DgDetalle)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox CboApe;
+        private System.Windows.Forms.DataGridView DgPedidos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox LblNumero;
+        private System.Windows.Forms.DataGridView DgDetalle;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox LblMonto;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox CboNom;
+    }
+}
+
---
+++ Propuesto01/Session_02/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Session_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection cn = new SqlConnection(ConfigurationManager.ConnectionStrings["Leon"].ConnectionString);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ListaAnios();
+            ListaNombres();
+        }
+
+
+        public void ListaAnios()
+        {
+            using (SqlCommand cmd = new SqlCommand("Usp_Lista_Apellidos", cn))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    da.SelectCommand = cmd;
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    using (DataSet df = new DataSet())
+                    {
+                        da.Fill(df, "Usp_Lista_Apellidos");
+                        CboApe.DataSource = df.Tables["Usp_Lista_Apellidos"];
+                        CboApe.DisplayMember = "Apellidos";
+                        CboApe.ValueMember = "Apellidos";
+                    }
+                }
+            }
+        }
+
+
+        public void ListaNombres()
+        {
+            using (SqlCommand cmd = new SqlCommand("Usp_Lista_Nombres", cn))
+            {
+                using (SqlDataAdapter da = new SqlDataAdapter())
+                {
+                    da.SelectCommand = cmd;
+                    da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    using (DataSet df = new DataSet())
+                    {
+                        da.Fill(df, "Usp_Lista_Nombres");
+                        CboNom.DataSource = df.Tables["Usp_Lista_Nombres"];
+                        CboNom.DisplayMember = "Nombre";
+                        CboNom.ValueMember = "Nombre";
+                    }
+                }
+            }
+        }
+
+        private void CboAnios_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            using (SqlCommand cmd = new SqlCommand("Usp_Lista_Busqueda", cn))
+            {
+                using (SqlDataAdapter Da = new SqlDataAdapter())
+                {
+                    Da.SelectCommand = cmd;
+                    Da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    Da.SelectCommand.Parameters.AddWithValue("@apellidos", CboApe.SelectedValue);
+                    using (DataSet df = new DataSet())
+                    {
+                        Da.Fill(df, "Empleados");
+                        DgPedidos.DataSource = df.Tables["Empleados"];
+                        LblNumero.Text = df.Tables["Empleados"].Rows.Count.ToString();
+                    }
+                }
+            }
+        }
+
+        private void DgPedidos_DoubleClick(object sender, EventArgs e)
+        {
+            object Codigo;
+            Codigo = DgPedidos.CurrentRow.Cells[0].Value;
+            using (SqlCommand cmd = new SqlCommand("Usp_Lista_Busqueda", cn))
+            {
+                using (SqlDataAdapter Da = new SqlDataAdapter())
+                {
+                    Da.SelectCommand = cmd;
+                    Da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    Da.SelectCommand.Parameters.AddWithValue("@anio", Codigo);
+                    using (DataSet df = new DataSet())
+                    {
+                        Da.Fill(df, "Empleados");
+                        DgDetalle.DataSource = df.Tables["Empleados"];
+                        LblMonto.Text = df.Tables["Empleados"].ToString();
+                    }
+                }
+            }
+        }
+
+        private void CboNom_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            using (SqlCommand cmd = new SqlCommand("Usp_Lista_Busqueda", cn))
+            {
+                using (SqlDataAdapter Da = new SqlDataAdapter())
+                {
+                    Da.SelectCommand = cmd;
+                    Da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                    Da.SelectCommand.Parameters.AddWithValue("@nombres", CboNom.SelectedValue);
+                    using (DataSet df = new DataSet())
+                    {
+                        Da.Fill(df, "Empleados");
+                        DgPedidos.DataSource = df.Tables["Empleados"];
+                        LblNumero.Text = df.Tables["Empleados"].Rows.Count.ToString();
+                    }
+                }
+            }
+        }
+    }
+}
